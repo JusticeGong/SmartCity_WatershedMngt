6ab1ca1c192ff3eab81f51a210faec4af6a71729
Justice Gong <justice.gong@hotmail.com>
2017-05-01 16:42:46 -0400

    1

--- watershed/GeoRSSGenerate.py
+++ watershed/GeoRSSGenerate.py
@@ -1,12 +1,19 @@
 from lxml import etree as ET
-# from watershed import models
 
-root = ET.Element('background')
-starttime = ET.SubElement(root, 'starttime')
-hour = ET.SubElement(starttime, 'hour')
-# allw = Watershed.objects.get(watershedID="020600030406")
-# print(allw)
-# hour.text = allw
-#
-#
-# print(ET.tostring(root, pretty_print=True, xml_declaration=True))
+import _mysql
+
+db=_mysql.connect(host="smartcity1.cwir7vtofu6m.us-west-2.rds.amazonaws.com", user="bpasmartcity1", passwd="pass1234", db="Watershed", port =3306)
+
+c=
+
+c.execute("""SELECT * FROM Watershed""")
+
+# root = ET.Element('background')
+# starttime = ET.SubElement(root, 'starttime')
+# hour = ET.SubElement(starttime, 'hour')
+# # allw = Watershed.objects.get(watershedID="020600030406")
+# # print(allw)
+# # hour.text = allw
+# #
+# #
+# # print(ET.tostring(root, pretty_print=True, xml_declaration=True))
