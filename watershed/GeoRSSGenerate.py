2d3cfe32541bf0a577c7fe11512b865aaadf558b
Justice Gong <justice.gong@hotmail.com>
2017-05-02 15:02:35 -0400

    Python Script GeoRSS

--- watershed/GeoRSSGenerate.py
+++ watershed/GeoRSSGenerate.py
@@ -26,18 +26,18 @@ try:
     from xml.sax.saxutils import escape
 
     with open('GeoRSS.xml', encoding='utf-8', mode='w') as f:
-        f.write('<?xml version="1.0" encoding="utf-8">\n')
-        f.write('<title>Watershed_NaturalFeature</title>')
-        f.write('<feed>')
+        f.write('<?xml version="1.0" encoding="utf-8"?>\n')
+        f.write('<feed>\n')
+        f.write('<title>Watershed_NaturalFeature</title>\n')
         for i in range(cursor.rowcount):
             row = cursor.fetchone()
-            f.write('<entry>')
-            f.write('<title>' + row[2] + '</title>')
-            f.write('<id>' + row[1] + '</id>')
-            f.write('<summary>' + row[3] + '</summary>')
-            f.write('<georss:point>' + row[4] + ' ' + row[5] + '</georss:point>')
-            f.write('</entry>')
-        f.write('</feed>')
+            f.write('<entry>\n')
+            f.write('<title>' + row[2] + '</title>\n')
+            f.write('<id>' + row[1] + '</id>\n')
+            f.write('<summary>' + row[3] + '</summary>\n')
+            f.write('<georss:point>' + row[4] + ' ' + row[5] + '</georss:point>\n')
+            f.write('</entry>\n')
+        f.write('</feed>\n')
         f.close()
 finally:
     # Don't forget to close DB connection
